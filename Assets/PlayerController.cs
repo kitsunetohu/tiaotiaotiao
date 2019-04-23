5be632b3d55c56c3f25f8d21d6636cfa1c9b3ae3
congxi <[bofenghui@gmail.com]>
2019-04-23 15:35:18 +0900

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonsterLove.StateMachine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator playerAnimator;
+    public enum PlayerStates{
+        wait,
+        run,
+        jump,
+        die,
+    }
+
+    StateMachine<PlayerStates> playerFsm;
+
+    void Awake (){
+         playerFsm=StateMachine<PlayerStates>.Initialize(this);
+         
+    }
+    void Start(){
+        playerAnimator=GetComponent<Animator>();
+        playerFsm.ChangeState(PlayerStates.wait);
+    }
+
+    void wait_Enter(){
+        playerAnimator.Play("Standing@loop");
+    }
+    void wait_Updata(){
+        if(Input.GetAxisRaw("Horizontal")!=0){
+            
+        }
+    }
+
+}
