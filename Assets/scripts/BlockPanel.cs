57b42287eedd04120f3078d78b09eb61d4948d0e
congxi <bofenghui@gmail.com>
2019-04-28 21:44:54 +0900

    0

--- Assets/scripts/BlockPanel.cs
+++ Assets/scripts/BlockPanel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MonsterLove.StateMachine;
+using UnityEngine.UI;
 
 
 public class BlockPanel : MonoBehaviour
@@ -15,42 +16,86 @@ public class BlockPanel : MonoBehaviour
     [SerializeField]
     public LayerMask shelf;
     StateMachine<ChoosingState> fsm;
-    GameObject choosingBlock;
+    GameObject chosingBlock;
+    Button chosingButton;
     public List<GameObject> blockList;
+    public List<int> _blockNumber;
+    public List<Button> blockButtons;
     public List<int> blockNumber;
+     int buttonNum;
     // Start is called before the first frame update
     void Start()
     {
         fsm = StateMachine<ChoosingState>.Initialize(this);
         fsm.ChangeState(ChoosingState.Prepare);
+        InitializeNum();
+        RefreshNum();       
     }
 
     void Choosing_Enter()
     {
-        if (choosingBlock != null)
+        if (chosingBlock != null)
         {   //put object behind camera
-            choosingBlock= Instantiate(choosingBlock,
+            chosingBlock = Instantiate(chosingBlock,
             Camera.main.gameObject.transform.position - 100 * Camera.main.gameObject.transform.forward,
             Quaternion.identity);
         }
 
     }
 
-    void Choosing_Update(){
+    void Choosing_Update()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if(Physics.Raycast(ray,out hit,100,shelf)){
-            choosingBlock.transform.position=hit.point;
+        if (Physics.Raycast(ray, out hit, 100, shelf))
+        {
+            chosingBlock.transform.position = hit.point;
+            if (Input.GetMouseButtonUp(0))
+            {//松开切换状态
+                blockNumber[buttonNum] -= 1;
+                RefreshNum();
+                fsm.ChangeState(ChoosingState.Prepare);
+                chosingBlock = null;
+            }
         }
-        if(Input.GetMouseButtonUp(0)){
-            fsm.ChangeState(ChoosingState.Prepare);
-        }
+
 
     }
     public void PushButtonDown(int buttonNum)
-    {
-        choosingBlock = blockList[buttonNum];
+    {//バトンをタッチしたら、バトンからバトンの番号がもらえます。
+        if (blockNumber[buttonNum] <= 0)
+        {
+            return;//もしブロックがないなら、リターン
+        }
+        chosingButton = blockButtons[buttonNum];
+        chosingBlock = blockList[buttonNum];
+        this.buttonNum = buttonNum;
         Debug.Log("button" + buttonNum);
         fsm.ChangeState(ChoosingState.Choosing);
+    }
+
+    void RefreshNum()
+    {
+        try
+        {
+            for (int i=0;i<blockNumber.Count;i++)
+            {
+                string buttonNumInText = System.Convert.ToString(blockNumber[i]);
+                blockButtons[i].GetComponentInChildren<Text>().text=buttonNumInText;
+            }
+
+        }
+        catch (System.ArgumentNullException)
+        {
+            System.Console.WriteLine("String is null");
+        }
+    }
+
+    void InitializeNum(){
+        blockNumber=new List<int>();
+        for (int i = 0; i < _blockNumber.Count; i++)
+        {//初期化
+            blockNumber.Add(_blockNumber[i]) ;
+        }
     }
 }
