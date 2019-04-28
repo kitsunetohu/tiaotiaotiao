6c7b35189b92c46b22d68e59b3f064be83cf0869
congxi <bofenghui@gmail.com>
2019-04-28 10:42:25 +0900

    0

---
+++ Assets/scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //经过后是否消失
+    public bool detoryOnTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        
+    }
+
+}
